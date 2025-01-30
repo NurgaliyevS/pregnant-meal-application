fe24031df1bc0e1ba0e3380d5bc67dbc0c14a1a5
NurgaliyevS <nurgasab@gmail.com>
2025-01-30 21:31:14 +0500

    upd

--- pages/blog/BlogPostContent.jsx
+++ pages/blog/BlogPostContent.jsx
@@ -28,18 +28,18 @@ function BlogPostContent({ post, relatedPosts }) {
   return (
     <div className="mx-auto">
       <Head>
-        <title>{`${post.title} | MVPAgency Blog`}</title>
+        <title>{`${post.title} | PregnantMeal Blog`}</title>
         <meta name="description" content={post.excerpt} />
         <meta name="keywords" content={post.tags.join(", ")} />
         <link
           rel="canonical"
-          href={`https://mvpagency.org/blog/${post.slug}`}
+          href={`https://pregnantmeal.com/blog/${post.slug}`}
         />
-        <meta property="og:title" content={`${post.title} | MVPAgency Blog`} />
+        <meta property="og:title" content={`${post.title} | PregnantMeal Blog`} />
         <meta property="og:description" content={post.excerpt} />
         <meta
           property="og:url"
-          content={`https://mvpagency.org/blog/${post.slug}`}
+          content={`https://pregnantmeal.com/blog/${post.slug}`}
         />
         <meta property="og:type" content="article" />
         <meta property="article:published_time" content={post.date} />
