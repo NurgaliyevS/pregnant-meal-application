1fe91e20219b644bbb7722743b16760167b7faf1
NurgaliyevS <nurgasab@gmail.com>
2025-02-22 21:51:07 +0500

    upd

--- next.config.js
+++ next.config.js
@@ -4,13 +4,23 @@ const { withPlausibleProxy } = require('next-plausible');
 const nextConfig = withPlausibleProxy()({
   reactStrictMode: true,
   images: {
-    domains: ['0d3810ffb029f275b4526ce60d3098d7.r2.cloudflarestorage.com'],
+    domains: [
+      '0d3810ffb029f275b4526ce60d3098d7.r2.cloudflarestorage.com',
+      'pregnantmeal.com',  // Add your domain
+      'localhost:3000'
+    ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "0d3810ffb029f275b4526ce60d3098d7.r2.cloudflarestorage.com",
         port: "",
         pathname: "/pregnant-meal-images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pregnantmeal.com",
+        port: "",
+        pathname: "/**",
       }
     ],
   },
