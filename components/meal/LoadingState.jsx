86d092cf7fd4104375778634aad5e8cee0310190
NurgaliyevS <nurgasab@gmail.com>
2025-02-22 21:19:49 +0500

    upd

--- components/meal/LoadingState.jsx
+++ components/meal/LoadingState.jsx
@@ -1,8 +1,8 @@
 function LoadingState() {
   return (
-    <div className="text-center">
+    <div className="text-center min-h-[640px]">
       <div className="spinner"></div>
-      <h2 className="text-2xl font-bold mb-4">Loading meal plan...</h2>
+      <h2 className="text-2xl font-bold mb-4 mt-20">Loading meal plan...</h2>
     </div>
   );
 }
